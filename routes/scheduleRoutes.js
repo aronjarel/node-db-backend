c3638ddd1ffecad347561a6c63758d1abd23c868
aronjarel <aron.jarel@gmail.com>
2024-10-15 09:44:12 -0400

    Added missing sql module

--- routes/scheduleRoutes.js
+++ routes/scheduleRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { poolPromise } = require('../db');
+const { sql, poolPromise } = require('../db');
 
 // Get all schedules
 router.get('/', async (req, res) => {
